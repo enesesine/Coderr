90144db1373cdd25b7d0368841221853d0b952ae
Enes <eneslucker@gmail.com>
2025-06-12 14:45:30 +0200

    login endpoint

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, LoginSerializer
+
 
 class RegistrationView(APIView):
     permission_classes = []  # Offen
@@ -19,4 +20,21 @@ class RegistrationView(APIView):
                 'email': user.email,
                 'user_id': user.id,
             }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    permission_classes = []  # Offen
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'username': user.username,
+                'email': user.email,
+                'user_id': user.id,
+            }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
